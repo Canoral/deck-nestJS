bf3bd29e6ea07ddf0a629ccb3c6e5727c5beafe3
Canoral <kaya68@outlook.com>
2024-02-16 00:51:59 +0100

    Configuration de typeORM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CardModule } from 'src/card/card.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import config from 'commons/config';
 
 @Module({
-  imports: [CardModule],
+  imports: [
+    ConfigModule.forRoot({ load: [config], isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'deck',
+      password: 'deck',
+      database: 'deck',
+      entities: [],
+    }),CardModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/card/card.controller.ts
+++ src/card/card.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CardService } from './card.service';
+import { promises } from 'dns';
 
 @Controller('card')
-export class CardController {}
+export class CardController {
+  constructor(private readonly cardService: CardService) {}
+
+  @Get()
+  getAllCards() {
+    return this.cardService.getAll()
+}}
